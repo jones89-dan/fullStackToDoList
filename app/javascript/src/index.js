7e11f0322299c5c9fd239e73b5da280cd9243106
jones89-dan <jones.89.dan@gmail.com>
2022-04-18 21:52:59 -0700

    deploye to heroku

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -7,7 +7,7 @@ import {
 
 indexTasks(function (response) {
   var htmlString = response.tasks.map(function(task) {
-    return "<div class='col-12 m-4 panel-info text-center p-2 border rounded task' data-id='" + task.id + "'> \
+    return "<div class='col-12 panel-info text-center p-2 border rounded task' data-id='" + task.id + "'> \
       " + task.content + "\
       </div>";
   });
