461693fb09ff86289b2470e0b37856b7b1951938
jones89-dan <jones.89.dan@gmail.com>
2022-04-20 13:08:50 -0700

    Adjusted delete to reflect without manual page reload

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -7,28 +7,26 @@ import {
   deleteTask,
 } from "./requests.js";
 
-indexTasks(function (response) {
-  var htmlString = response.tasks.map(function(task) {
-    return "<div class='col-12 panel-info text-center p-2 border rounded task' data-id='" + task.id + "'> \
-      <button type='button' class='float-left btn btn-outline-primary btn-sm'>Complete</button>" + task.content + "\
-      <button type='button' class='delete float-right btn btn-outline-primary btn-sm'>Delete</button></div>";
 
-    });
+  indexTasks(function (response) {
+    var htmlString = response.tasks.map(function(task) {
+      return "<div class='col-12 panel-info text-center p-2 border rounded task' data-id='" + task.id + "'> \
+        <button type='button' class='float-left btn btn-outline-primary btn-sm'>Complete</button>" + task.content + "\
+        <button type='button' class='delete float-right btn btn-outline-primary btn-sm'>Delete</button></div>";
 
-  $("#tasks").html(htmlString);
+      });
 
-  $('#create-task').on('submit', function (e) {
-      postTask();
-    });
+    $("#tasks").html(htmlString);
 
-  //$(document).on('click', '.delete', function () {
-  //  deleteTask($(delete).data("id"));
-  //});
+    $('#create-task').on('submit', function (e) {
+        postTask();
+      });
+
 
   $('.delete').click(function(e){
     var id = $(e.target).parent().attr("data-id");
       deleteTask(id);
-      indexTasks();
+      window.location.reload();
       console.log("id= " + id);
   });
 
---
+++ public/packs/js/index-5d3a5821f3917a778cdf.js
@@ -0,0 +1,116 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/packs/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./app/javascript/packs/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./app/javascript/packs/index.js":
+/*!***************************************!*\
+  !*** ./app/javascript/packs/index.js ***!
+  \***************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var src_index__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! src/index */ "./app/javascript/src/index.js");
+/* harmony import */ var src_index__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(src_index__WEBPACK_IMPORTED_MODULE_0__);
+
+
+/***/ }),
+
+/***/ "./app/javascript/src/index.js":
+/*!*************************************!*\
+  !*** ./app/javascript/src/index.js ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/djones/altCademy/Ruby/fswd-todolist/app/javascript/src/index.js: Missing semicolon. (10:14)\n\n   8 | } from \"./requests.js\";\n   9 |\n> 10 | var startIndex () {\n     |               ^\n  11 |   indexTasks(function (response) {\n  12 |     var htmlString = response.tasks.map(function(task) {\n  13 |       return \"<div class='col-12 panel-info text-center p-2 border rounded task' data-id='\" + task.id + \"'> \\\n    at instantiate (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:72:32)\n    at constructor (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:358:12)\n    at Parser.raise (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:3334:19)\n    at Parser.semicolon (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:3995:10)\n    at Parser.parseVarStatement (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:15086:10)\n    at Parser.parseStatementContent (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:14631:21)\n    at Parser.parseStatement (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:14556:17)\n    at Parser.parseBlockOrModuleBlockBody (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:15195:25)\n    at Parser.parseBlockBody (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:15186:10)\n    at Parser.parseProgram (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:14474:10)\n    at Parser.parseTopLevel (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:14461:25)\n    at Parser.parse (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:16420:10)\n    at parse (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/parser/lib/index.js:16472:38)\n    at parser (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/core/lib/parser/index.js:52:34)\n    at parser.next (<anonymous>)\n    at normalizeFile (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/core/lib/transformation/normalize-file.js:87:38)\n    at normalizeFile.next (<anonymous>)\n    at run (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/core/lib/transformation/index.js:31:50)\n    at run.next (<anonymous>)\n    at Function.transform (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/core/lib/transform.js:25:41)\n    at transform.next (<anonymous>)\n    at step (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/gensync/index.js:261:32)\n    at /Users/djones/altCademy/Ruby/fswd-todolist/node_modules/gensync/index.js:273:13\n    at async.call.result.err.err (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/gensync/index.js:223:11)\n    at /Users/djones/altCademy/Ruby/fswd-todolist/node_modules/gensync/index.js:189:28\n    at /Users/djones/altCademy/Ruby/fswd-todolist/node_modules/@babel/core/lib/gensync-utils/async.js:74:7\n    at /Users/djones/altCademy/Ruby/fswd-todolist/node_modules/gensync/index.js:113:33\n    at step (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/gensync/index.js:287:14)\n    at /Users/djones/altCademy/Ruby/fswd-todolist/node_modules/gensync/index.js:273:13\n    at async.call.result.err.err (/Users/djones/altCademy/Ruby/fswd-todolist/node_modules/gensync/index.js:223:11)");
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=index-5d3a5821f3917a778cdf.js.map
