6fb59833623911f4e3d6a74d286762bdfc42c709
jones89-dan <jones.89.dan@gmail.com>
2022-04-26 13:15:33 -0700

    Changed logic for the complete/activate buttons

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -11,8 +11,8 @@ import {
 
   indexTasks(function (response) {
     var htmlString = response.tasks.map(function(task) {
-      return "<div class='col-12 panel-info text-center p-2 mb-2 border rounded task " + (task.completed ? 'green' : '') + "' data-id='" + task.id + "'> \
-        <button type='button' class='activate float-left btn btn-outline-dark btn-sm mr-2'>Active</button> \
+      return "<div class='col-12 panel-info text-center p-2 mb-2 border rounded task " + (task.completed ? 'green' : 'activetask') + "' data-id='" + task.id + "'> \
+        <button type='button' class='activate float-left btn btn-outline-dark btn-sm mr-2'>Activate</button> \
         <button type='button' class='complete float-left btn btn-outline-dark btn-sm'>Complete</button>" + task.content + " \
         <button type='button' class='delete float-right btn btn-outline-dark btn-sm'>Delete</button></div>";
       });
@@ -37,12 +37,14 @@ import {
     var id = $(e.target).parent().attr("data-id");
     completeTask(id);
       $(this).parent().addClass("green");
+      $(this).parent().removeClass("activetask");
   });
 
   $('.activate').click(function(e){
     var id = $(e.target).parent().attr("data-id");
     activateTask(id);
     $(this).parent().removeClass("green");
+    $(this).parent().addClass("activetask");
   });
 
 });
